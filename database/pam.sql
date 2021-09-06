89febac401988cc8ae7a3a8cb7086f373b7f80a4
rezalmp7 <rezalwahyu10@gmail.com>
2021-09-06 15:47:44 +0700

    dev

--- database/pam.sql
+++ database/pam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2021 at 11:23 AM
+-- Generation Time: Sep 06, 2021 at 10:47 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -52,7 +52,7 @@ CREATE TABLE `pelanggan` (
   `id` int(10) NOT NULL,
   `id_pelanggan` varchar(10) CHARACTER SET latin1 NOT NULL,
   `nama` char(40) NOT NULL,
-  `dusun` char(40) NOT NULL,
+  `dusun` int(4) NOT NULL,
   `rt` int(3) NOT NULL,
   `rw` int(3) NOT NULL,
   `no_rumah` int(4) NOT NULL,
@@ -67,7 +67,9 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id`, `id_pelanggan`, `nama`, `dusun`, `rt`, `rw`, `no_rumah`, `no_hp`, `level`, `code`, `qrcode`) VALUES
-(1, 'AKL000001', 'Rezal Wahyu Pratama', 'Angkatan Lor', 1, 4, 15, '087721191226', 'pelanggan', 'gyy8m', 'qrcode_AKL000001_ebcead84b386e809429366544d0b3d5f.png');
+(1, 'AKL000001', 'Rezal Wahyu Pratama', 1, 2, 2, 1, '087721191226', 'pelanggan', '62rc5', 'qrcode_AKL000001_ebcead84b386e809429366544d0b3d5f.png'),
+(2, 'KDL000002', 'Rezal Wahyu', 3, 2, 2, 1, '087721191226', 'pelanggan', '7ddn6', 'qrcode_KDL000002_ea3429699807126986797a820838b04e.png'),
+(3, 'AKL000003', 'uzumaki naruto', 1, 1, 1, 3, '432472947293', 'pelanggan', 'j4gmp', 'qrcode_AKL000003_9e2ff343ada81cde67fd87c23f9deff1.png');
 
 -- --------------------------------------------------------
 
@@ -81,33 +83,29 @@ CREATE TABLE `tagihan` (
   `id_pelanggan` varchar(10) NOT NULL,
   `nama` char(40) NOT NULL,
   `tgl_cek` date NOT NULL,
-  `bulan` date NOT NULL,
   `awal` int(3) NOT NULL,
-  `bln_terbilang` varchar(30) NOT NULL,
   `akhir` int(3) NOT NULL,
   `pemakaian` int(5) NOT NULL,
   `tagihan` int(7) NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `metode_bayar` varchar(30) NOT NULL,
-  `tgl_bayar` date DEFAULT NULL,
-  `petugas_verifikasi` varchar(50) NOT NULL,
+  `status` enum('butuh bayar','butuh konfirmasi','lunas') NOT NULL,
+  `metode_bayar` enum('transfer','petugas') DEFAULT NULL,
+  `tgl_bayar` timestamp NULL DEFAULT NULL,
+  `petugas_verifikasi` varchar(50) DEFAULT NULL,
   `file` varchar(500) DEFAULT NULL,
-  `waktu` time NOT NULL DEFAULT current_timestamp(),
-  `foto_meteran` varchar(500) NOT NULL
+  `foto_meteran` varchar(500) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tagihan`
 --
 
-INSERT INTO `tagihan` (`id`, `id_tagihan`, `id_pelanggan`, `nama`, `tgl_cek`, `bulan`, `awal`, `bln_terbilang`, `akhir`, `pemakaian`, `tagihan`, `status`, `metode_bayar`, `tgl_bayar`, `petugas_verifikasi`, `file`, `waktu`, `foto_meteran`) VALUES
-(14, 'BLR010204-2021-01-08', 'BLR010204', 'Abdul', '2021-01-08', '2021-01-01', 131, 'January 2021', 156, 25, 40500, 'LUNAS', '', NULL, '', NULL, '00:00:00', ''),
-(15, 'BLR010204-2021-02-06', 'BLR010204', 'Abdul', '2021-02-06', '2021-02-01', 156, 'February 2021', 175, 19, 31500, 'LUNAS', '', NULL, '', NULL, '00:00:00', ''),
-(16, 'BLR010204-2021-03-31', 'BLR010204', 'Abdul', '2021-03-31', '2021-03-01', 175, 'March 2021', 190, 15, 25500, 'LUNAS', '', NULL, '', '../files/3. UPGRIS.png', '00:00:00', ''),
-(19, 'SDW020315-2021-01-08', 'SDW020315', 'Yos Aipassa', '2021-01-08', '2021-01-01', 115, 'January 2021', 129, 14, 24000, 'LUNAS', '', NULL, '', NULL, '00:00:00', ''),
-(20, 'SDW020315-2021-02-04', 'SDW020315', 'Yos Aipassa', '2021-02-04', '2021-02-01', 129, 'February 2021', 134, 5, 10500, 'LUNAS', '', NULL, '', NULL, '00:00:00', ''),
-(85, 'BLR020378-2021-05-01', 'BLR020378', 'Aros Roso', '2021-05-01', '2021-05-01', 10, 'May 2021', 90, 80, 123000, 'PERLU DIBAYAR', 'BELUM DIPILIH', NULL, '', NULL, '00:00:00', 'BLR020378-2021-05-01.png'),
-(108, 'BLR010339-2021-04-25', 'BLR010339', 'Wondo', '2021-04-25', '2021-04-01', 1, 'April 2021', 12, 11, 19500, 'PERLU DIBAYAR', 'BELUM DIPILIH', NULL, '', NULL, '23:37:16', 'BLR010339-2021-04-25.jpg');
+INSERT INTO `tagihan` (`id`, `id_tagihan`, `id_pelanggan`, `nama`, `tgl_cek`, `awal`, `akhir`, `pemakaian`, `tagihan`, `status`, `metode_bayar`, `tgl_bayar`, `petugas_verifikasi`, `file`, `foto_meteran`, `create_at`) VALUES
+(1, 'AKL000001-04-09-2021', '1', 'Rezal Wahyu Pratama', '2021-09-29', 1, 12, 11, 27000, 'lunas', 'transfer', '2021-09-05 02:33:30', 'admin', 'AKL000001-04-09-2021.png', 'AKL000001-04-09-2021.jpeg', '2021-09-04 15:42:31'),
+(2, 'KDL000002-04-09-2021', '2', 'Rezal Wahyu', '2021-09-28', 1, 12, 11, 27000, 'butuh bayar', '', NULL, NULL, NULL, 'KDL000002-04-09-2021.jpeg', '2021-09-04 19:43:01'),
+(3, 'AKL000001-05-09-2021', '1', 'Rezal Wahyu Pratama', '2021-10-08', 12, 15, 3, 11000, 'butuh konfirmasi', '', '2021-09-05 17:57:31', NULL, 'AKL000001-05-09-2021.jpeg', 'AKL000001-05-09-2021.jpeg', '2021-09-05 10:54:00'),
+(4, 'AKL000003-06-09-2021', '3', 'uzumaki naruto', '2021-09-06', 0, 12, 12, 29000, 'lunas', 'transfer', '2021-09-06 03:38:53', 'admin', 'AKL000003-06-09-2021.jpg', 'AKL000003-06-09-2021.jpg', '2021-09-06 08:33:56'),
+(5, 'AKL000003-06-09-2021', '3', 'uzumaki naruto', '2021-10-06', 12, 15, 3, 11000, 'butuh bayar', '', NULL, NULL, NULL, 'AKL000003-06-09-2021.jpg', '2021-09-06 08:35:19');
 
 -- --------------------------------------------------------
 
@@ -179,7 +177,7 @@ ALTER TABLE `pelanggan`
 -- AUTO_INCREMENT for table `tagihan`
 --
 ALTER TABLE `tagihan`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
